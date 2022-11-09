373923c2f19dde7f3c4199820a804a4114f143dd
Calvin Walker <calvinwalker@Calvins-MacBook-Pro.local>
2022-11-09 16:17:33 -0600

    tests

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from scraping.scraper import Ncaa
 if __name__ == "__main__":
     nc = Ncaa()
     school_ids = nc.school_id_grabber()
-    print(school_ids)
     stats = nc.stats(school_ids)
     print(stats)
 
