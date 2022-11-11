456c6754e8746063d7121d3d058268125a53a765
Calvin Walker <calvinwalker@Calvins-MacBook-Pro.local>
2022-11-10 18:10:13 -0600

    timer

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from scraping.scraper import Ncaa
+import time
 
 if __name__ == "__main__":
+    start_time = time.time()
     nc = Ncaa()
     school_ids = nc.school_id_grabber()
     stats = nc.stats(school_ids)
     print(stats)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
